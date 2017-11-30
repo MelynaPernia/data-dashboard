742b3c621c15ec172b73035082fba0c9b605bb4a
mebipeos <mebipeos@outlook.com>
2017-11-30 01:22:21 -0500

    Modificando los tabs

--- js/app.js
+++ js/app.js
@@ -4,97 +4,55 @@ window.addEventListener('load', function(event) {
   var countryPeru = document.querySelector('#peru');
   var countryMexico = document.querySelector('#mexico');
 
-  function showCountry(event){
-    //console.log(event.target.nextElementSibling);
+  function showCountry(event) {
+  // console.log(event.target.nextElementSibling);
     event.target.nextElementSibling.classList.toggle('display-none');
-
-
-
   }
 
 
-  countryChile.addEventListener('click',showCountry);
-  countryPeru.addEventListener('click',showCountry);
-  countryMexico.addEventListener('click',showCountry);
+  countryChile.addEventListener('click', showCountry);
+  countryPeru.addEventListener('click', showCountry);
+  countryMexico.addEventListener('click', showCountry);
+
+  /* TABS */
+
+  var tabs = document.querySelectorAll('.tab');
+
+  var overviewSection = document.querySelector('#overview');
+  var studentsSection = document.querySelector('#students');
+  var teachersSection = document.querySelector('#teachers');
+
+  function showTab(event) {
+    var tab = event.target.textContent;
+    switch (true) {
+    case (tab === 'Overview'):
+      overviewSection.classList.remove('display-none');
+      studentsSection.classList.add('display-none');
+      teachersSection.classList.add('display-none');
+      break;
+    case (tab === 'Students'):
+      overviewSection.classList.add('display-none');
+      studentsSection.classList.remove('display-none');
+      teachersSection.classList.add('display-none');
+      break;
+    case (tab === 'Teachers'):
+      overviewSection.classList.add('display-none');
+      studentsSection.classList.add('display-none');
+      teachersSection.classList.remove('display-none');
+      break;
+    default:
+      overviewSection.classList.add('display-none');
+      studentsSection.classList.add('display-none');
+      teachersSection.classList.add('display-none');
+      break;
+    }
+  }
+
+
+  console.log(tabs);
+  for (var i = 0;i < tabs.length;i++) {
+    tabs[i].addEventListener('click', showTab);
+  }
 
 
 });
-
-
-// window.addEventListener('load', function(event) {
-//
-// });
-/* Dropdwon de menu */
-  // var menu = document.querySelector('.menu');
-  // var country = document.querySelectorAll('.country');
-  // var city = document.querySelectorAll('.city');
-  // function showCountry() {
-  //   for (var i = 0; i < country.length; i++) {
-  //     switch (true) {
-  //       case 'Chile':
-  //           country.classList.toggle('display-none');
-  //         break;
-  //
-  //       default:
-  //
-  //     }
-  //   }
-  // }
-  // function showCity() {
-  //
-  //
-  // }
-
-
-/* Tabs */
-// var tab = document.querySelectorAll('.tab');
-//
-//
-// function showTab(event) {
-//   // console.log(event.target.dataset.selectedTab);
-//   selectedTab = event.target.dataset.selectedTab;
-//   var overView = document.querySelector('#over-view');
-//   var students = document.querySelector('#students');
-//   var teacher = document.querySelector('#teachers');
-//
-//   switch (selectedTab) {
-//   case 'tabOverView':
-//     students.style.displayNone;
-//     // teacher.style.displayNone;
-//     break;
-//
-//   case 'tabStudents':
-//     // overView.style.displayNone;
-//     // teacher.style.displayNone;
-//     break;
-//
-//   case 'tabTeacher':
-//     // overView.style.displayNone;
-//     // students.style.displayNone;
-//     break;
-//   default:
-//     // overView.style.displayNone;
-//     // students.style.displayNone;
-//     // teacher.style.displayNone;
-//     break;
-//   }
-// }
-// function loadPage() {
-//   var tab = document.querySelectorAll('.tab');
-//   for (var i = 0; i < tab.length; i++) {
-//     tab[i].addEventListener('click', showTab);
-//   }
-// }
-//
-//
-// loadPage();
-
-// Agregar fotos de coders y nombres
-// function addCoder() {
-//   var coderPhoto;
-//   var coderName;
-//   var coderTechSkills;
-//   var coderLifeSkills;
-//   console.log(data[]);
-// }
-// addCoder();
